bba1d88fd6ef819a4a447a7778c9cb4ad5930c63
Roland <rrim1368@scs.ubbcluj.ro>
2016-05-11 19:58:49 +0300

    modify

--- index.js
+++ index.js
@@ -31,10 +31,6 @@ server.get("/demo/getexample", function(req, res) {
     var respRandomValue3 = Math.floor(Math.random() * 10 + 1) ;
     console.log('request  description: ', respRandomValue); 
     res.json({type: true, data: 'A:'+ respRandomValue+' '+respRandomValue2+' '+respRandomValue3});
-
-
-    // prepare the json answer
-    //res.json({type: true, data: 'Answer for the get Answer '});
 });
 
 
